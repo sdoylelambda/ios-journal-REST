9c14b006ed1e097ff2a54de96ce322b8664be357
Sean Doyle <sdoylelambda@gmail.com>
2019-01-31 17:06:27 -0500

    Basic storyboard with tableVC to show title, timestamp and text. As well as a DetailVC with segues and outlets.

---
+++ Journal REST 1.31/Journal REST 1.31/EntryTableViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleText: UILabel!
+    @IBOutlet weak var timeStamp: UILabel!
+    @IBOutlet weak var textField: UILabel!
+    
+}
