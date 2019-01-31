e56c3031d0631a6423e37d39410e9f475c0a5a6b
Sean Doyle <sdoylelambda@gmail.com>
2019-01-31 16:37:26 -0500

    Initial commit.

---
+++ Journal REST 1.31/Journal REST 1.31/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal REST 1.31
+//
+//  Created by Lambda_School_Loaner_18 on 1/31/19.
+//  Copyright © 2019 SeanDoyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
