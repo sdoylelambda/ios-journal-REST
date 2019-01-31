9df9bfa32540546b697b8d546e309fa75517479b
Sean Doyle <sdoylelambda@gmail.com>
2019-01-31 18:18:37 -0500

    API skeleton code.

--- Journal REST 1.31/Journal REST 1.31/Entry.swift
+++ Journal REST 1.31/Journal REST 1.31/Entry.swift
@@ -1,8 +1,16 @@
 import Foundation
 
-struct Entry: Codable {
-    var identifier: String
+struct Entry: Codable, Equatable {
+    let identifier: String
     var title: String
     var bodyText: String
     var timeStamp: Date
+    
+    // may need to initialize time stamp with default value??
+    init(identifier: String, title: String, bodyText: String, timeStamp: Date) {
+        self.identifier = identifier
+        self.title = title
+        self.bodyText = bodyText
+        self.timeStamp = timeStamp
+    }
 }
---
+++ Journal REST 1.31/Journal REST 1.31/EntryController.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+class EntryController {
+    
+ //   var entries: [Entries] = []
+    private var entry: Entry?
+    
+    typealias completetionHandler = (Error?) -> Void
+    
+    var baseURL = URL(string: "https://console.firebase.google.com/u/0/project/journal-f50bf/database/journal-f50bf/data")!
+    
+    func create() {
+        
+    }
+    
+    
+    func put(searchTerm: String, resultType: Entry, completion: @escaping completetionHandler) {
+        
+      //  URLSession.shared.dataTask(with: baseURL) { (data, _, error)  {
+        let firebase = baseURL.appendPathComponent(searchTerm)
+      
+        
+        
+        
+        if let data = data {
+            let jsonDecoder = JSONDecoder()
+            do {
+                self.entry = try jsonDecoder.decode(entry, from: data)
+            } catch {
+                print(error)
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+                return
+            }
+        } else {
+            DispatchQueue.main.async {
+                completion(NSError())
+            }
+            return
+            }
+        } .resume()
+    }
+}
